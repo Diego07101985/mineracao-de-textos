d9dbc70475b45ea68601c4271ff536c0ea125514
dsalles <dsalles@corp.globo.com>
2019-10-23 19:41:21 -0300

    Mineracao texto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pdf_service_manager import ServiceManagerPdf
 from vector_space_similarity import ServiceTextMining
 from models import PdfOutput, DocumentSimilary
+from classification.lazy import Knn
+from rating_classifier import Rating
 
 
 import numpy as np
@@ -12,67 +14,113 @@ import glob2
 
 
 def main():
-    files = []
-    # 1 - Conjunto de dados curto
-    fileNames = sorted(glob2.glob("curto/*.txt"))
+
+    # query = "HAL is a multi-disciplinary open access"
+    # query = "Part-of-speech (PoS ) tagging is one of the earliest steps of NLP. However, in spite of its linguistic nature, linguistic studies comparing the impact of diﬀerent"
+    fileNames = sorted(glob2.glob(
+        "./docs/NLI/portuguese_native_language.txt"))
     for filename in fileNames:
-        files.append(open(filename, "r+").read())
+        query = open(filename, "r+").read()
 
     serviceTextMining = ServiceTextMining()
-    print("Tópico 1 - Matrix TF * IDF")
-    terms = serviceTextMining.select_terms(files)
-    matriz_tf = serviceTextMining.create_matriz_itf_terms(terms)
-    matriz_df = serviceTextMining.create_matriz_idf_terms(terms, files)
-    matriz_tf_df = serviceTextMining.create_matriz_tf_df_terms(matriz_tf, matriz_df)
+    distance = serviceTextMining.calc_euclidian_distance_in_query(query)
+    print("distance {0}".format(len(distance)))
 
-    print("Criando arquivo csv")
-    csv_file = open("trabalho3.csv", "w+")
-    csv_manages = csv.writer(csv_file, delimiter=",", quoting=csv.QUOTE_MINIMAL)
-    csv_file.write("@terms")
-    csv_file.write("\n")
-    csv_manages.writerow(terms)
-    csv_file.write("@matriz_tf")
-    csv_file.write("\n")
-    for line in range(len(matriz_tf)):
-        csv_manages.writerow(matriz_tf[line])
-    csv_file.write("@matriz_df")
-    csv_file.write("\n")
-    csv_manages.writerow(matriz_df)
-    csv_file.write("@matriz_df_idf")
-    csv_file.write("\n")
-    for line in range(len(matriz_tf_df)):
-        csv_manages.writerow(matriz_tf_df[line])
+    knn = Knn()
+    nmin = knn.NminElements(distance, 3)
 
-    # Deve
+    fileNames = sorted(glob2.glob("./docs/**/*.txt"))
 
-    # Com o	conjunto de	dados longo
-    query = "but kate's job  , if you will "
+    classes = []
+    for fileName in fileNames:
+        clasz = fileName.split("/")[2]
+        if clasz not in classes:
+            classes.append(clasz)
 
-    print("Calculo distancia dos cossenos a partir de uma query = {0}".format(query))
-    distance = serviceTextMining.calc_distance_cosseno_in_query(query)
+    rt = Rating()
+    rt.set_class_predict("NLI")
+    print(rt.confusion_matrix(nmin, fileNames, classes))
+    print(rt.precision_value(nmin, fileNames, classes))
+    print(rt.recall_score(nmin, fileNames, classes))
+    print(rt.precision_recall_fscore_support(nmin, fileNames, classes))
 
-    print("Tópico 2 - 10 Documentos mais relevantes")
-    nmax = serviceTextMining.Nmaxelements(distance, 10)
+    # print("distance {0}".format(len(distance)))
+    # for document in distance:
+    #     print("Documento {0} distancia dos cossenos {1}  Classe {2}".format(
+    #         document.file, document.coeficient, document.clasz
+    #     ))
 
-    for document in nmax:
+    for document in nmin:
         print(
-            "Documento {0} distancia dos cossenos {1}".format(
-                document.file, document.coeficient
+            "Documento {0} distancia dos cossenos {1}  Classe {2}".format(
+                document.file, document.coeficient, document.clasz
             )
         )
-    pdfOutput = PdfOutput(
-        None,
-        np.transpose(matriz_tf),
-        matriz_df,
-        np.transpose(matriz_tf_df),
-        terms,
-        fileNames,
-        nmax,
-        query,
-    )
 
-    servicePdfManager = ServiceManagerPdf()
-    servicePdfManager.writePdf(pdfOutput)
+    # files = []
+    # # 1 - Conjunto de dados curto
+    # fileNames = sorted(glob2.glob("curto/*.txt"))
+    # for filename in fileNames:
+    #     files.append(open(filename, "r+").read())
+
+    # serviceTextMining = ServiceTextMining()
+    # print("Tópico 1 - Matrix TF * IDF")
+    # terms = serviceTextMining.select_terms(files)
+    # matriz_tf = serviceTextMining.create_matriz_itf_terms(terms)
+    # matriz_df = serviceTextMining.create_matriz_idf_terms(terms, files)
+    # matriz_tf_df = serviceTextMining.create_matriz_tf_df_terms(
+    #     matriz_tf, matriz_df)
+
+    # print("Criando arquivo csv")
+    # csv_file = open("trabalho3.csv", "w+")
+    # csv_manages = csv.writer(csv_file, delimiter=",",
+    #                          quoting=csv.QUOTE_MINIMAL)
+    # csv_file.write("@terms")
+    # csv_file.write("\n")
+    # csv_manages.writerow(terms)
+    # csv_file.write("@matriz_tf")
+    # csv_file.write("\n")
+    # for line in range(len(matriz_tf)):
+    #     csv_manages.writerow(matriz_tf[line])
+    # csv_file.write("@matriz_df")
+    # csv_file.write("\n")
+    # csv_manages.writerow(matriz_df)
+    # csv_file.write("@matriz_df_idf")
+    # csv_file.write("\n")
+    # for line in range(len(matriz_tf_df)):
+    #     csv_manages.writerow(matriz_tf_df[line])
+
+    # # Deve
+
+    # # Com o	conjunto de	dados longo
+    # query = "but kate's job  , if you will "
+
+    # print(
+    #     "Calculo distancia dos cossenos a partir de uma query = {0}".format(query))
+    # distance = serviceTextMining.calc_distance_cosseno_in_query(query)
+
+    # print("Tópico 2 - 10 Documentos mais relevantes")
+    # nmax = serviceTextMining.Nmaxelements(distance, 10)
+
+    # for document in nmax:
+    #     print(
+    #         "Documento {0} distancia dos cossenos {1}".format(
+    #             document.file, document.coeficient
+    #         )
+    #     )
+    # pdfOutput = PdfOutput(
+    #     None,
+    #     np.transpose(matriz_tf),
+    #     matriz_df,
+    #     np.transpose(matriz_tf_df),
+    #     terms,
+    #     fileNames,
+    #     nmax,
+    #     query,
+    # )
+
+    # servicePdfManager = ServiceManagerPdf()
+    # servicePdfManager.writePdf(pdfOutput)
 
 
 if __name__ == "__main__":
--- vector_space_similarity.py
+++ vector_space_similarity.py
@@ -3,7 +3,12 @@ from nltk.tokenize import word_tokenize
 from numpy import ndarray
 import math
 from nltk.tokenize import RegexpTokenizer
-from models import DocumentSimilary
+from models import DocumentSimilary, DocumentSimilaryEuclidian
+from nltk.corpus import stopwords
+from string import punctuation
+from nltk.stem import WordNetLemmatizer
+
+
 import glob2
 
 
@@ -11,25 +16,43 @@ class ServiceTextMining:
 
     _docs = []
 
+    def strip_punctuation(self, str):
+        return ''.join(c for c in str if c not in punctuation)
+
+    def strip_number(self, str):
+        return ''.join(c for c in str if not c.isdigit())
+
     def select_terms(self, files):
         array_terms = []
         self._docs = []
         pattern = "\w+"
         tokenizer = RegexpTokenizer(pattern)
+        stop_words = stopwords.words('english')
+        wordnet_lemmatizer = WordNetLemmatizer()
+
+        filtered_sentence = []
 
         for i in range(len(files)):
             base_text = files[i]
+            # pre processamento
+            text_without_pontuaction = self.strip_punctuation(base_text)
+            text_without_number = self.strip_number(text_without_pontuaction)
+            word_tokens = word_tokenize(text_without_number)
+            lemmatized_word = [wordnet_lemmatizer.lemmatize(
+                word) for word in word_tokens]
+            removing_stopwords = [
+                word for word in lemmatized_word if word not in stop_words]
+
             if i > 0:
-                terms = tokenizer.tokenize(base_text)
-                self._docs.append(terms)
-                for newterm in terms:
+                self._docs.append(removing_stopwords)
+                for newterm in removing_stopwords:
                     if newterm not in array_terms:
                         array_terms.append(newterm)
             else:
-                terms = tokenizer.tokenize(base_text)
-                self._docs.append(terms)
-                for term in set(terms):
+                self._docs.append(removing_stopwords)
+                for term in set(removing_stopwords):
                     array_terms.append(term)
+
         return array_terms
 
     def create_matriz_binary_terms(self, terms):
@@ -47,7 +70,7 @@ class ServiceTextMining:
             column = []
         return np.transpose(matriz_terms)
 
-    def cosine_similarity(self, files, matriz_terms):
+    def cosine_similarity(self, matriz_terms):
         count_el_mtz = 0
         numerador = 0
         denominador = 0
@@ -62,7 +85,8 @@ class ServiceTextMining:
                 second_doc = matriz_terms[count_el_mtz + 1]
 
                 for indice_term in range(len(first_doc)):
-                    numerador += first_doc[indice_term] * second_doc[indice_term]
+                    numerador += first_doc[indice_term] * \
+                        second_doc[indice_term]
                     element += math.pow(first_doc[indice_term], 2)
                     element2 += math.pow(second_doc[indice_term], 2)
 
@@ -70,11 +94,34 @@ class ServiceTextMining:
                 if numerador == 0 and denominador == 0:
                     similaridade_cossenos.append(0)
                 else:
-                    similaridade_cossenos.append(round(numerador / denominador, 3))
+                    similaridade_cossenos.append(
+                        round(numerador / denominador, 3))
 
             count_el_mtz += 1
 
         return similaridade_cossenos
+
+    def euclidian_distance(self, matriz_terms):
+
+        # d(p,q)=d(q,p) = raiz((q1-p1)ˆ2 +(q2-p2))
+        count_el_mtz = 0
+        euclidian_distance_terms = []
+        calc_between_point = 0
+
+        while count_el_mtz < len(matriz_terms):
+            first_doc = matriz_terms[count_el_mtz]
+
+            if count_el_mtz + 1 < len(matriz_terms):
+                second_doc = matriz_terms[count_el_mtz + 1]
+
+                for indice_term in range(len(first_doc)):
+                    calc_between_point += math.pow(first_doc[indice_term] -
+                                                   second_doc[indice_term], 2)
+
+                euclidian_distance_terms.append(
+                    round(math.sqrt(calc_between_point), 3))
+            count_el_mtz += 1
+        return euclidian_distance_terms
 
     def create_matriz_itf_terms(self, terms):
         matriz_terms = []
@@ -109,7 +156,8 @@ class ServiceTextMining:
             for doc in self._docs:
                 if term in doc:
                     column.append(
-                        round(sum([term == doc[i] for i in range(len(doc))])), 3
+                        round(sum([term == doc[i]
+                                   for i in range(len(doc))])), 3
                     )
                 else:
                     column.append(0)
@@ -155,9 +203,41 @@ class ServiceTextMining:
 
         return result
 
+    def calc_euclidian_distance_in_query(self, query):
+        files = []
+        fileNames = sorted(glob2.glob("./docs/**/*.txt"))
+
+        for filename in fileNames:
+            files.append(open(filename, "r+").read())
+
+        print("Files {0}".format(len(fileNames)))
+        count = 0
+        coefient_doc = []
+        documents = []
+        coeficient_array = []
+
+        while count < len(fileNames):
+            serviceTextMining = ServiceTextMining()
+            terms = serviceTextMining.select_terms([files[count], query])
+            relevation_matriz_docs = serviceTextMining.create_matriz_tf_df_terms(
+                serviceTextMining.create_matriz_itf_terms(terms),
+                serviceTextMining.create_matriz_idf_terms(terms, files),
+            )
+            eu_distance = (
+                serviceTextMining.euclidian_distance(relevation_matriz_docs
+                                                     ),
+            )
+
+            document = DocumentSimilaryEuclidian(
+                fileNames[count], eu_distance[0], fileNames[count].split("/")[2])
+            documents.append(document)
+            count += 1
+        print("Files {0}".format(len(documents)))
+        return documents
+
     def calc_distance_cosseno_in_query(self, query):
         files = []
-        fileNames = sorted(glob2.glob("longo/*.txt"))
+        fileNames = sorted(glob2.glob("./docs/**/*.txt"))
         for filename in fileNames:
             files.append(open(filename, "r+").read())
         count = 0
@@ -173,9 +253,8 @@ class ServiceTextMining:
                 serviceTextMining.create_matriz_idf_terms(terms, files),
             )
             cosine_similarity = (
-                serviceTextMining.cosine_similarity(
-                    [files[count], query], relevation_matriz_docs
-                ),
+                serviceTextMining.cosine_similarity(relevation_matriz_docs
+                                                    ),
             )
 
             document = DocumentSimilary(fileNames[count], cosine_similarity[0])
@@ -195,15 +274,14 @@ class ServiceTextMining:
 
         while count < len(files):
             serviceTextMining = ServiceTextMining()
-            terms = serviceTextMining.select_terms([files[count], files[count - 1]])
+            terms = serviceTextMining.select_terms(
+                [files[count], files[count - 1]])
             relevation_matriz_docs = serviceTextMining.create_matriz_tf_df_terms(
                 serviceTextMining.create_matriz_itf_terms(terms),
                 serviceTextMining.create_matriz_idf_terms(terms, files),
             )
             cosine_similarity = (
-                serviceTextMining.cosine_similarity(
-                    [files[count], files[count - 1]], relevation_matriz_docs
-                ),
+                serviceTextMining.cosine_similarity(relevation_matriz_docs),
             )
             document = DocumentSimilary(
                 [fileNames[count], files[count - 1]], cosine_similarity
@@ -236,4 +314,3 @@ class ServiceTextMining:
                 selected_names.append(selected_document.file)
 
         return final_list
-
